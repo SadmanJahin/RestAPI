1c37f0ad3b012bdccb1e6bcaf09bb8555898f2f9
Sadman Jahin <sadmanjahin115@gmail.com>
2022-01-07 17:38:19 +0600

    Update urls.py

--- Prediction/urls.py
+++ Prediction/urls.py
@@ -18,15 +18,15 @@ from django.urls import path
 from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import url
+from django.urls import include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls')),
     
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 if settings.DEBUG:
