ecb320ef294276b6e4c2731ec7b3d31cf03066fe
Sadman Jahin <sadmanjahin115@gmail.com>
2022-02-25 18:03:10 +0600

    Update views.py

--- api/views.py
+++ api/views.py
@@ -74,17 +74,27 @@ def upload(request):
 	return Response(serializer.data)
 
 @api_view(['POST'])
+@api_view(['GET'])
 def analyseHeartbeat(request):
     
-    serializer = FileSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
+    #serializer = FileSerializer(data=request.data)
+    #if serializer.is_valid():
+        #serializer.save()
         
-    name="media/documents/"+request.POST['title']
-    data,sr=librosa.load(name)
-    mfccs = librosa.feature.mfcc(y=data, sr=sr)
-    mfccs_scaled_feature=np.mean(mfccs.T,axis=0)
-    return HttpResponse(sr)
+    #name="media/documents/"+request.POST['title']
+    #data,sr=librosa.load(name)
+    #mfccs = librosa.feature.mfcc(y=data, sr=sr)
+    #mfccs_scaled_feature=np.mean(mfccs.T,axis=0)
+    import pickle
+    loaded_model = pickle.load(open("SVM.sav", 'rb'))
+    loaded_scaler= pickle.load(open("scale.pkl", 'rb'))
+    data,sample_rate=librosa.load("predict.wav")
+    mfccs = np.mean(librosa.feature.mfcc(y=data, sr=sample_rate, n_mfcc=40).T,axis=0)
+    mfccs = loaded_scaler.transform([mfccs])
+    predict = loaded_model.predict(mfccs)
+    
+    
+    return HttpResponse(predict)
 #def upload(request):
  #   if request.method=='POST':
 #       title=request.POST['title']
