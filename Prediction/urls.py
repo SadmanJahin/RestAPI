35305939725408b8c17ade0d7f4295f92fedbceb
Sadman Jahin <sadmanjahin115@gmail.com>
2022-06-16 19:01:14 +0600

    Update urls.py

--- Prediction/urls.py
+++ Prediction/urls.py
@@ -19,9 +19,10 @@ from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, re_path
-
+from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.overview,name="overview"),
     path('api/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls')),
     
